462009e08003b0ad50c4766e94a9f86f8921f255
ojiepermana <me@ojiepermana.com>
2023-12-15 13:02:47 +0700

    bui

--- README.md
+++ README.md
@@ -21,6 +21,7 @@ git submodule add --branch main  https://github.com/etos-systems/spv-angular-web
 git submodule add --branch main  https://github.com/etos-systems/auth-angular-web.git code/indonusa/angular/mfe/auth <br />
 git submodule add --branch main  https://github.com/etos-systems/contract-angular-web.git code/indonusa/angular/mfe/contract <br />
 git submodule add --branch main  https://github.com/etos-systems/budget-angular-web.git code/indonusa/angular/mfe/budget <br />
+git submodule add --branch main  https://github.com/etos-systems/invoice-angular-web.git code/indonusa/angular/mfe/invoice <br />
 
 
 ## Indonusa Laravel Services
@@ -28,4 +29,5 @@ git submodule add --branch main  https://github.com/etos-systems/auth-laravel-se
 git submodule add --branch main  https://github.com/etos-systems/contract-laravel-services.git code/indonusa/laravel/services/contract <br />
 git submodule add --branch main  https://github.com/etos-systems/master_laravel_services.git code/indonusa/laravel/services/master <br />
 git submodule add --branch main  https://github.com/etos-systems/budget-laravel-services.git code/indonusa/laravel/services/budget <br />
+git submodule add --branch main  https://github.com/etos-systems/invoice-laravel-services.git code/indonusa/laravel/services/invoice <br />
 
---
+++ image/php-swoole/8.3/build.sh
@@ -0,0 +1,169 @@
+docker build -t etos8.3 .
+
+
+docker tag etos8.3 asia.gcr.io/etos-cloud/etos
+docker push asia.gcr.io/etos-cloud/etos
+
+docker tag etos8.3 asia.gcr.io/etos-cloud/user
+docker push asia.gcr.io/etos-cloud/user
+
+docker tag etos8.3 asia.gcr.io/etos-cloud/website
+docker push asia.gcr.io/etos-cloud/website
+
+docker tag etos8.3 asia.gcr.io/etos-cloud/accounting
+docker push asia.gcr.io/etos-cloud/accounting
+
+docker tag etos8.3 asia.gcr.io/etos-cloud/sales
+docker push asia.gcr.io/etos-cloud/sales
+
+docker tag etos8.3 asia.gcr.io/etos-cloud/payroll
+docker push asia.gcr.io/etos-cloud/payroll
+
+docker tag etos8.3 asia.gcr.io/etos-cloud/supervisor
+docker push asia.gcr.io/etos-cloud/supervisor
+
+docker tag etos8.3 asia.gcr.io/etos-cloud/operator
+docker push asia.gcr.io/etos-cloud/operator
+
+
+
+
+# penawawaran
+docker tag etos8.3 asia.gcr.io/etos-cloud/penawawaran
+docker push asia.gcr.io/etos-cloud/penawawaran
+
+# pelanggan
+docker tag etos8.3 asia.gcr.io/etos-cloud/pelanggan
+docker push asia.gcr.io/etos-cloud/pelanggan
+
+# kontrak
+docker tag etos8.3 asia.gcr.io/etos-cloud/kontrak
+docker push asia.gcr.io/etos-cloud/kontrak
+
+# penjualan
+docker tag etos8.3 asia.gcr.io/etos-cloud/penjualan
+docker push asia.gcr.io/etos-cloud/penjualan
+
+# tagihan
+docker tag etos8.3 asia.gcr.io/etos-cloud/tagihan
+docker push asia.gcr.io/etos-cloud/tagihan
+
+# distribusi
+docker tag etos8.3 asia.gcr.io/etos-cloud/distribusi
+docker push asia.gcr.io/etos-cloud/distribusi
+
+# pembayaran
+docker tag etos8.3 asia.gcr.io/etos-cloud/pembayaran
+docker push asia.gcr.io/etos-cloud/pembayaran
+
+# faktur
+docker tag etos8.3 asia.gcr.io/etos-cloud/faktur
+docker push asia.gcr.io/etos-cloud/faktur
+
+# pekerjaan
+docker tag etos8.3 asia.gcr.io/etos-cloud/pekerjaan
+docker push asia.gcr.io/etos-cloud/pekerjaan
+
+# jadwal
+docker tag etos8.3 asia.gcr.io/etos-cloud/jadwal
+docker push asia.gcr.io/etos-cloud/jadwal
+
+# logistik
+docker tag etos8.3 asia.gcr.io/etos-cloud/logistik
+docker push asia.gcr.io/etos-cloud/logistik
+
+# Work Oder
+docker tag etos8.3 asia.gcr.io/etos-cloud/wo
+docker push asia.gcr.io/etos-cloud/wo
+
+# Mapping
+docker tag etos8.3 asia.gcr.io/etos-cloud/mapping
+docker push asia.gcr.io/etos-cloud/mapping
+
+# Laporan Pelanggan
+docker tag etos8.3 asia.gcr.io/etos-cloud/laporan-pelanggan
+docker push asia.gcr.io/etos-cloud/laporan-pelanggan
+
+# Anggaran
+docker tag etos8.3 asia.gcr.io/etos-cloud/anggaran
+docker push asia.gcr.io/etos-cloud/anggaran
+
+# Purchasing
+docker tag etos8.3 asia.gcr.io/etos-cloud/purchasing
+docker push asia.gcr.io/etos-cloud/purchasing
+
+# Payment
+docker tag etos8.3 asia.gcr.io/etos-cloud/payment
+docker push asia.gcr.io/etos-cloud/payment
+
+# PPH23
+docker tag etos8.3 asia.gcr.io/etos-cloud/pph23
+docker push asia.gcr.io/etos-cloud/pph23
+
+# BNI HOST
+docker tag etos8.3 asia.gcr.io/etos-cloud/bni-host
+docker push asia.gcr.io/etos-cloud/bni-host
+
+# BCA HOST
+docker tag etos8.3 asia.gcr.io/etos-cloud/bca-host
+docker push asia.gcr.io/etos-cloud/bca-host
+
+# BNI OGP
+docker tag etos8.3 asia.gcr.io/etos-cloud/bni-ogp
+docker push asia.gcr.io/etos-cloud/bni-ogp
+
+# Cash Management
+docker tag etos8.3 asia.gcr.io/etos-cloud/cash-management
+docker push asia.gcr.io/etos-cloud/cash-management
+
+# Keluhan
+docker tag etos8.3 asia.gcr.io/etos-cloud/keluhan
+docker push asia.gcr.io/etos-cloud/keluhan
+
+# Kontak pelanggan
+docker tag etos8.3 asia.gcr.io/etos-cloud/pelanggan
+docker push asia.gcr.io/etos-cloud/pelanggan
+
+# inspeksi
+docker tag etos8.3 asia.gcr.io/etos-cloud/inspeksi
+docker push asia.gcr.io/etos-cloud/inspeksi
+
+# Training pelanggan
+docker tag etos8.3 asia.gcr.io/etos-cloud/training-pelanggan
+docker push asia.gcr.io/etos-cloud/training-pelanggan
+
+# Pendampingan Audit
+docker tag etos8.3 asia.gcr.io/etos-cloud/pendampingan-audit
+docker push asia.gcr.io/etos-cloud/pendampingan-audit
+
+# Reqruetment
+docker tag etos8.3 asia.gcr.io/etos-cloud/requirement
+docker push asia.gcr.io/etos-cloud/requirement
+
+# Pegawai
+docker tag etos8.3 asia.gcr.io/etos-cloud/pegawai
+docker push asia.gcr.io/etos-cloud/pegawai
+
+# Evaluasi
+docker tag etos8.3 asia.gcr.io/etos-cloud/evaluasi
+docker push asia.gcr.io/etos-cloud/evaluasi
+
+# training
+docker tag etos8.3 asia.gcr.io/etos-cloud/training
+docker push asia.gcr.io/etos-cloud/training
+
+# Aset
+docker tag etos8.3 asia.gcr.io/etos-cloud/aset
+docker push asia.gcr.io/etos-cloud/aset
+
+# Gudang
+docker tag etos8.3 asia.gcr.io/etos-cloud/gudang
+docker push asia.gcr.io/etos-cloud/gudang
+
+# Helpdesk
+docker tag etos8.3 asia.gcr.io/etos-cloud/helpdesk
+docker push asia.gcr.io/etos-cloud/helpdesk
+
+# Notification
+docker tag etos8.3 asia.gcr.io/etos-cloud/notification
+docker push asia.gcr.io/etos-cloud/notification
