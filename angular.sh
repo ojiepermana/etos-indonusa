fe38d331f5bde3afa7acc921fea948f865188dee
ojiepermana <me@ojiepermana.com>
2024-01-11 20:11:30 +0700

    simpanan

--- angular.sh
+++ angular.sh
@@ -1,1 +1,66 @@
-bash code/indonusa/angular/app/accounting/update.sh
+## accounting app
+# cd code/indonusa/angular/app/accounting/
+# bash update.sh
+# npm run hosting
+# cd $(pwd)
+
+
+# # host app
+# cd code/indonusa/angular/app/host/
+# bash update.sh
+# npm run hosting
+# cd $(pwd)
+
+
+# # payroll app
+# cd code/indonusa/angular/app/payroll/
+# bash update.sh
+# npm run hosting
+# cd $(pwd)
+
+
+# # report app
+# cd code/indonusa/angular/app/report/
+# bash update.sh
+# npm run hosting
+# cd $(pwd)
+
+# #sales app
+# cd code/indonusa/angular/app/sales/
+# bash update.sh
+# npm run hosting
+# cd $(pwd)
+
+# #spv app
+# cd code/indonusa/angular/app/spv/
+# bash update.sh
+# npm run hosting
+# cd $(pwd)
+
+
+# #auth mfe
+# cd code/indonusa/angular/mfe/auth/
+# bash update.sh
+# npm run hosting
+# cd $(pwd)
+
+
+# #budget mfe
+# cd code/indonusa/angular/mfe/budget/
+# bash update.sh
+# npm run hosting
+# cd $(pwd)
+
+
+#invoice mfe
+# cd code/indonusa/angular/mfe/invoice/
+# bash update.sh
+# npm run hosting
+# cd $(pwd)
+
+
+#share mfe
+cd code/indonusa/angular/mfe/share/
+bash update.sh
+npm run hosting
+cd $(pwd)
