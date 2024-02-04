890f87b9759020e2a5fe7fa5a6702842aa5d05ab
ojiepermana <me@ojiepermana.com>
2024-02-04 20:40:24 +0700

    clear

--- angular.sh
+++ angular.sh
@@ -1,66 +1,66 @@
 #auth mfe
 cd code/indonusa/angular/mfe/auth/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 
 #share mfe
 cd code/indonusa/angular/mfe/share/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 # accounting app
 cd code/indonusa/angular/app/accounting/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 
 # host app
 cd code/indonusa/angular/app/host/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 
 # # payroll app
 # cd code/indonusa/angular/app/payroll/
-# bash update.sh
-# #npm run hosting
+# #bash update.sh
+# npm run hosting
 # cd /root/project/etos-indonusa
 
 
 # report app
 cd code/indonusa/angular/app/report/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 # management app
 cd code/indonusa/angular/app/management/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 #sales app
 cd code/indonusa/angular/app/sales/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 #spv app
 cd code/indonusa/angular/app/spv/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 
 #crm app
 cd code/indonusa/angular/app/crm/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 
@@ -68,39 +68,39 @@ cd /root/project/etos-indonusa
 
 #budget mfe
 cd code/indonusa/angular/mfe/budget/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 #offer mfe
 cd code/indonusa/angular/mfe/contract/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 #invoice mfe
 cd code/indonusa/angular/mfe/invoice/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 
 #faktur mfe
 cd code/indonusa/angular/mfe/faktur/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 
 
-#offer mfe
-cd code/indonusa/angular/mfe/offer/
-bash update.sh
-#npm run hosting
+#accounting mfe
+cd code/indonusa/angular/app/accounting/
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
 
 #transaction mfe
 cd code/indonusa/angular/mfe/transaction/
-bash update.sh
-#npm run hosting
+#bash update.sh
+npm run hosting
 cd /root/project/etos-indonusa
