f0606a3a13fb3253afb7891a284138ab267da53b
ojiepermana <me@ojiepermana.com>
2024-02-21 17:54:26 +0700

    aas

--- image/php-swoole/8.1/build.sh
+++ image/php-swoole/8.1/build.sh
@@ -39,49 +39,49 @@ docker push asia.gcr.io/etos-cloud/etos-erp-penawaran
 
 # ERP - BQ
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-bq
-docker push asia.gcr.io/etos-cloud/etos-erp-b1
+docker push asia.gcr.io/etos-cloud/etos-erp-bq
 
 # ERP - Kontrak
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-kontrak
-docker push asia.gcr.io/etos-cloud/etos-erp-kontrak
+docker push asia.gcr.io/etos-cloud/erp-kontrak
 
 # ERP - Penjualan
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-penjualan
-docker push asia.gcr.io/etos-cloud/etos-erp-penjualan
+docker push asia.gcr.io/etos-cloud/erp-penjualan
 
 # ERP - Jadwal
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-jadwal
-docker push asia.gcr.io/etos-cloud/etos-erp-jadwal
+docker push asia.gcr.io/etos-cloud/erp-jadwal
 
 # ERP - Pekerjaan
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-pekerjaan
-docker push asia.gcr.io/etos-cloud/etos-erp-pekerjaan
+docker push asia.gcr.io/etos-cloud/erp-pekerjaan
 
 # ERP - EQS
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-eqs
-docker push asia.gcr.io/etos-cloud/etos-erp-eqs
+docker push asia.gcr.io/etos-cloud/erp-eqs
 
 
 # ERP - Finance
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-finance
-docker push asia.gcr.io/etos-cloud/etos-erp-finance
+docker push asia.gcr.io/etos-cloud/erp-finance
 
 
 # ERP - HR
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-hr
-docker push asia.gcr.io/etos-cloud/etos-erp-hr
+docker push asia.gcr.io/etos-cloud/erp-hr
 
 
 # EPP - GA
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-ga
-docker push asia.gcr.io/etos-cloud/etos-erp-ga
+docker push asia.gcr.io/etos-cloud/erp-ga
 
 
 # ERP - Purchasing
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-purchasing
-docker push asia.gcr.io/etos-cloud/etos-erp-purchasing
+docker push asia.gcr.io/etos-cloud/erp-purchasing
 
 
 # ERP - Payment
 docker tag etos8.1 asia.gcr.io/etos-cloud/erp-payment
-docker push asia.gcr.io/etos-cloud/etos-erp-payment
+docker push asia.gcr.io/etos-cloud/erp-payment
